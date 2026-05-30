ba37453db3f3964e6f5b315ea10eb045fe22ea9e
HAN <macbookair@HAN-3.local>
2026-05-30 09:56:26 +0700

    up eror

--- cuti.php
+++ cuti.php
@@ -1,7 +1,4 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 // ==================================================================
 // 1. PENGATURAN LOG ERROR (PRODUCTION MODE)
 // ==================================================================
@@ -9,6 +6,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0); 
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/php_errors.log');
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 // 2. PROTEKSI FILE AUTHENTIKASI
 if (!file_exists('auth_checkwa.php')) {
